362a2b3d29f9ae74de193a026a5e52c1e26fe8fa
Maximilian-Stefan-Ernst <34346372+Maximilian-Stefan-Ernst@users.noreply.github.com>
2022-12-05 12:11:21 +0100

    Update Proximal.jl

--- src/diff/Proximal.jl
+++ src/diff/Proximal.jl
@@ -8,6 +8,19 @@ end
 SemOptimizerProximal(;algorithm = ProximalAlgorithms.PANOC(), options = Dict{Symbol, Any}(), operator_g, operator_h = nothing, kwargs...) = 
     SemOptimizerProximal(algorithm, options, operator_g, operator_h)
 
+############################################################################################
+### Recommended methods
+############################################################################################
+
+update_observed(optimizer::SemOptimizerProximal, observed::SemObserved; kwargs...) = optimizer
+
+############################################################################################
+### additional methods
+############################################################################################
+
+algorithm(optimizer::SemOptimizerProximal) = optimizer.algorithm
+options(optimizer::SemOptimizerProximal) = optimizer.options
+
 ############################################################################
 ### Pretty Printing
 ############################################################################
